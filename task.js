a2bcdad08bd659737b46832974d1728d320ed513
a.pushkin <a.v.pushkin@gmail.com>
2023-06-16 16:38:41 +0300

    completed task_3

--- task.js
+++ task.js
@@ -2,12 +2,26 @@ const form = document.getElementById('signin__form')
 const signin = document.getElementById('signin')
 const welcome = document.getElementById('welcome')
 const user_id = document.getElementById('user_id')
+const card = document.querySelector('.card')
+
+function addBtn() {
+    const deAuth = document.createElement('button')
+    deAuth.textContent = 'Выйти'
+    card.appendChild(deAuth)
+    deAuth.onclick = () => {
+        welcome.classList.remove('welcome_active')
+        signin.classList.add('signin_active')
+        deAuth.remove()
+        localStorage.removeItem('auth_user')
+        form.reset()
+    }
+}
 
 if (localStorage.getItem('auth_user')) {
     user_id.textContent = localStorage.getItem('auth_user')
     signin.classList.remove('signin_active')
     welcome.classList.add('welcome_active')
-
+    addBtn()
 }
 
 form.onsubmit = (e) => {
@@ -23,11 +37,12 @@ form.onsubmit = (e) => {
             user_id.textContent = JSON.parse(request.responseText)['user_id']
             welcome.classList.add('welcome_active')
             console.log(`Ответ сервера: ${request.responseText}\nКод ответа - ${request.status}`)
+            addBtn()
         } else {
             if (request.readyState === XMLHttpRequest.DONE) {
+                form.reset()
                 console.log(`Ответ сервера: ${request.responseText}\nКод ответа - ${request.status}`)
                 alert('«Неверный логин/пароль»')
-            }
-        }}
+            }}}
         request.send(formData)
 }
